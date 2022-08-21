9e9eec5240525ad8fba12153388de9f283c11283
Piojank <98431706+Piojank@users.noreply.github.com>
2022-08-21 12:02:49 +0200

    update src

--- src/index.js
+++ src/index.js
@@ -1,3 +1,9 @@
 import './css/styles.css';
 
+const query = selector => document.querySelector(selector);
+
+const searchBox = query('#search-box');
+const countryList = query('.country-list');
+const countryInfo = query('.country-info');
+
 const DEBOUNCE_DELAY = 300;
