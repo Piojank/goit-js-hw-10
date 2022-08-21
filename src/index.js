d5f1a995068ceb195af4f142f5b784d06b1649e4
Piojank <98431706+Piojank@users.noreply.github.com>
2022-08-21 13:24:57 +0200

    task update

--- src/index.js
+++ src/index.js
@@ -1,4 +1,7 @@
 import './css/styles.css';
+import { fetchCountries } from '../fetchcountries';
+import Notiflix from 'notiflix';
+import { debounce } from 'lodash';
 
 const query = selector => document.querySelector(selector);
 
@@ -7,3 +10,51 @@ const countryList = query('.country-list');
 const countryInfo = query('.country-info');
 
 const DEBOUNCE_DELAY = 300;
+
+Notiflix.Notify.info('Please type in correct country.');
+
+const renderCountriesList = countries => {
+  if (countries.length > 10) {
+    Notiflix.Notify.info('Too many matches, please enter more characters.');
+    listReset();
+  } else if (countries.length <= 10 && countries.length >= 2) {
+    listReset();
+
+    countries.map(({ name, flags }) => {
+      const liItem = document.createElement('li');
+      liItem.classList.add('country-list_item');
+      countryList.append(liItem);
+
+      const miniFlags = document.createElement('img');
+      miniFlags.src = flags.svg;
+      miniFlags.classList.add('flags-mini_img');
+      miniFlags.alt = 'country flag';
+      liItem.append(miniFlags);
+
+      const countryName = document.createElement('p');
+      countryName.textContent = name;
+      countryName.classList.add('country-list_name');
+      liItem.append(countryName);
+    });
+  } else if (countries.length === 1) {
+    listReset();
+
+    const finalCountry = countries.map(({ name, flags, capital, population, languages }) => {
+      return `<h1 class="country-info_name"><img src="${flags.svg}" class="flags-big_img"/>${name}</h1>
+      <p class="country-info_item"><span>Capital: </span> ${capital} </p>
+      <p class="country-info_item"><span>Population: </span> ${population} </p>
+      <p class="country-info_item"><span>Languages: </span> ${languages[0].name}</p>
+      `;
+    });
+    countryInfo.innerHTML = finalCountry;
+  } else if (countries.length < 1) {
+    Notiflix.Notify.info('Please type correct country.');
+  }
+};
+
+const listReset = () => {
+  countryList.innerHTML = '';
+  countryInfo.innerHTML = '';
+};
+
+searchBox.addEventListener('input', debounce(searchBoxValue, DEBOUNCE_DELAY));
