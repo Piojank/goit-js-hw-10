35d949bb031a921c843208cd673fb2dab2b4966b
piojank <p.jankowski931026@gmail.com>
2023-03-20 22:42:38 +0100

    Add searchBoxValue

--- src/index.js
+++ src/index.js
@@ -1,12 +1,28 @@
 import './css/styles.css';
 import Notiflix from 'notiflix';
+import { fetchCountries } from './js/fetchCountries';
+let debounce = require('lodash.debounce');
 
-
+const DEBOUNCE_DELAY = 300;
 
 const qs = s => document.querySelector(s);
 const searchBox = qs('#search-box');
 const countryList = qs('.country-list');
 const countryInfo = qs('.country-info');
 
-const DEBOUNCE_DELAY = 300;
+const searchBoxValue = () => {
+    fetchCountries(searchBox.value.trim())
+        .then((countries) => {
+            console.log(countries);
+            renderCountriesList(countries);
+        })
+        .catch((error) => console.log(error));
+};
 
+const renderCountriesList = () => {
+    
+}
+
+Notiflix.Notify.info('Please type in correct country.');
+
+searchBox.addEventListener("input", debounce(searchBoxValue, DEBOUNCE_DELAY));
--- src/js/fetchCountries.js
+++ src/js/fetchCountries.js
@@ -1,7 +1,7 @@
+const API = `https://restcountries.com/v3.1/name/${name}?fields=name,capital,population,flags,languages`;
+
 export function fetchCountries(name) {
-    return fetch(
-        `https://restcountries.com/v3.1/name/${name}?fields=name,capital,population,flags,languages`,
-    ).then((response) => {
+    return fetch(API).then((response) => {
         if (!response.ok) {
             throw new Error(response.status);
         }
