be3710f9f4d5a5ff6f0a2e318f7c0751d19b8cee
piojank <p.jankowski931026@gmail.com>
2023-03-20 22:11:06 +0100

    Add fetch

--- src/index.js
+++ src/index.js
@@ -1,3 +1,12 @@
 import './css/styles.css';
+import Notiflix from 'notiflix';
+
+
+
+const qs = s => document.querySelector(s);
+const searchBox = qs('#search-box');
+const countryList = qs('.country-list');
+const countryInfo = qs('.country-info');
 
 const DEBOUNCE_DELAY = 300;
+
