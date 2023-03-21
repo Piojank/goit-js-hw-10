99290f4d8e30c4a9fa6555f3eede1f4063ea91d3
piojank <p.jankowski931026@gmail.com>
2023-03-21 19:37:27 +0100

    update fetchCountries

--- src/index.js
+++ src/index.js
@@ -10,6 +10,8 @@ const searchBox = qs('#search-box');
 const countryList = qs('.country-list');
 const countryInfo = qs('.country-info');
 
+Notiflix.Notify.info('Please type in correct country.');
+
 const searchBoxValue = () => {
     fetchCountries(searchBox.value.trim())
         .then((countries) => {
@@ -19,10 +21,36 @@ const searchBoxValue = () => {
         .catch((error) => console.log(error));
 };
 
-const renderCountriesList = () => {
+const renderCountriesList = (countries) => {
     
-}
+    if (countries.length > 10) {
+        Notiflix.Notify.info('Too many matches found. Please enter a more specific name.');
+        listReset();
+    } else if (countries.length > 10 && countries.length >= 2) {
 
-Notiflix.Notify.info('Please type in correct country.');
+    countries.map(({ name, flags }) => {
+    const liItem = `
+        <li class="country-list_item">
+            <img src="${flags.svg}" alt="country flag" class="flags-mini_img">
+            <p class="country-list_name">${name}</p>
+        </li>
+    `;
+    return liItem;
+    });
+
+    } else if (countries.length === 1) {
+        listReset();
+
+
+    } else if (countries.length < 1) {
+        Notiflix.Notify.info('Please type correct country.');
+    }
+};
+
+const listReset = () => {
+    countryList.innerHTML = '';
+    countryInfo.innerHTML = '';
+};
 
 searchBox.addEventListener("input", debounce(searchBoxValue, DEBOUNCE_DELAY));
+
--- src/js/fetchCountries.js
+++ src/js/fetchCountries.js
@@ -1,6 +1,6 @@
-const API = `https://restcountries.com/v3.1/name/${name}?fields=name,capital,population,flags,languages`;
-
 export function fetchCountries(name) {
+    const API = `https://restcountries.com/v3.1/name/${name}?fields=name,capital,population,flags,languages`;
+
     return fetch(API).then((response) => {
         if (!response.ok) {
             throw new Error(response.status);
